019e74a0afff6eafb99f9e9d3201b034e8ceaabd
Craig MacLeod <c92macleod@gmail.com>
2021-05-12 12:00:51 +0100

    initial commit
    Co-authored-by: Newcoderca

---
+++ Queen-CraigChisha/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [health, setHealth] = useState(3)
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  )
+}
+
+export default App
