7926cbd83a3917fa28b763625f32d4427ce7169e
chisha <aamaechi@hotmail.com>
2021-05-13 17:03:27 +0100

    amending files duplicate json files

---
+++ src/GameScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Fetch from './Fetch'
+
+function GameScreen({score, setScore, name, profileData, setProfileData, gameStart}) {
+
+    // React.useEffect(() => {
+    //     setGameScore((gameScore) => gameScore+1)
+    // }, [score]);
+
+    return (
+    <>
+    {gameStart ? <Fetch setScore={setScore} name={name} profileData={profileData} setProfileData={setProfileData} gameStart={gameStart}/> : null}
+    <h2>Score: {score}</h2>
+    </>
+    )
+
+}
+
+export default GameScreen
---
+++ src/Profile.jsx
@@ -0,0 +1,28 @@
+// import React from "react";
+
+// const USER_URL = "https://api.github.com/users/";
+
+
+// function Profile(props) {
+//     const [profileData, setProfileData] = React.useState()
+
+//     React.useEffect(() => {
+//         fetch(USER_URL + props.name)
+//           .then((res) => res.json())
+//           .then((data) => setProfileData(data));
+//       }, [props.name]);
+    
+//       if (!profileData) {
+//         return <div>Loading...</div>;
+//       } else {
+//         return (
+//         <div>
+//             <h2>Hi, {profileData.login}!</h2>
+//             <img src={profileData.avatar_url} />
+//         </div>
+//         );
+//       }
+// }
+
+
+// export default Profile
